d9c33cd6ea0aea03fd475e17edd681cd249768c6
Pete Goodwin <pgoodwin@imekon.org>
2018-02-03 13:20:35 +0000

    found property to tween

--- breakout/brick.gd
+++ breakout/brick.gd
@@ -9,7 +9,7 @@ func _ready():
 	# Ensure this is in group 'brick' so we can detect it when the ball
 	# collides with it
 	add_to_group("brick")
-	tween.interpolate_property(sprite, "visibility/opacity", 1.0, 0.0, 0.3, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	tween.interpolate_property(sprite, "modulate:a", 1.0, 0.0, 0.3, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	tween.interpolate_property(sprite, "scale", sprite.get_scale(), Vector2(1.7, 1.7), 0.3, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	
 func getScore():
