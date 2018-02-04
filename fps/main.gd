ddebd1ffe0097b263ddbd7b4565fb9717ed58124
Pete Goodwin <github@imekon.org>
2018-02-04 09:08:29 +0000

    fps working!
    got a plain coloured ball
    got a textured floor
    got FPS back!

--- fps/main.gd
+++ fps/main.gd
@@ -1,7 +1,12 @@
 extends Spatial
 
-onready var fpsLabel = get_node("CanvasLayer/FPS")
+onready var fpsLabel = $CanvasLayer/FPS
 
 func _physics_process(delta):
-	fpsLabel.set_text("FPS: " + str(Performance.get_monitor(0)))
+	var fps = "FPS: " + str(Performance.get_monitor(Performance.TIME_FPS))
+	# var fps = "FPS: " + str(Engine.get_frames_per_second())
+	if fps != "":
+		fpsLabel.text = fps
+	else:
+		fpsLabel.text = "FPS is blank!"
 
