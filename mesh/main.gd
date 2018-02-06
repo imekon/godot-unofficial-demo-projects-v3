54835bc6e8d3043addac5de35eddd776d1e4019e
Pete Goodwin <pgoodwin@imekon.org>
2018-02-06 21:18:29 +0000

    expanded names

--- mesh/main.gd
+++ mesh/main.gd
@@ -10,16 +10,19 @@ func _ready():
 	createMesh(10, material)
 	
 func createMesh(size, material):
-	var st = SurfaceTool.new()
-	st.begin(Mesh.PRIMITIVE_TRIANGLES)
-	st.add_normal(Vector3( 0, 0, 1))
-	st.add_vertex(Vector3(-size, -size,  0))
-	st.add_vertex(Vector3( size,  size,  0))
-	st.add_vertex(Vector3( size, -size,  0))
-	st.add_vertex(Vector3(-size, -size,  0))
-	st.add_vertex(Vector3(-size,  size,  0))
-	st.add_vertex(Vector3( size,  size,  0))
-	var mesh = st.commit()
+	var surfaceTool = SurfaceTool.new()
+	surfaceTool.begin(Mesh.PRIMITIVE_TRIANGLES)
+	
+	surfaceTool.add_normal(Vector3(    0,     0,  1))
+	
+	surfaceTool.add_vertex(Vector3(-size, -size,  0))
+	surfaceTool.add_vertex(Vector3( size,  size,  0))
+	surfaceTool.add_vertex(Vector3( size, -size,  0))
+	surfaceTool.add_vertex(Vector3(-size, -size,  0))
+	surfaceTool.add_vertex(Vector3(-size,  size,  0))
+	surfaceTool.add_vertex(Vector3( size,  size,  0))
+	
+	var mesh = surfaceTool.commit()
 	meshInstance.mesh = mesh
 	meshInstance.set_surface_material(0, material)
 	
