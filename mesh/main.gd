b41c36df93897d91ee6bfdeb4b684888358659e8
Pete Goodwin <github@imekon.org>
2018-02-15 20:34:03 +0000

    got my weird texture working

--- mesh/main.gd
+++ mesh/main.gd
@@ -1,37 +1,35 @@
 extends Spatial
 
-onready var meshInstance = $Mesh
+onready var meshInstance = $MeshInstance
 
 var angle = 0
 
 func _ready():
-	var texture = load("res://junk.png")
-	var material = SpatialMaterial.new()
-	# material.albedo_color = Color(1.0, 0.0, 0.0)
-	material.albedo_texture = texture
-	createMesh(2, material)
+	var mat = SpatialMaterial.new()
+	mat.flags_unshaded = true
+	mat.albedo_texture = load("res://junk.png")
+	createMesh(mat, 2)
 	
-func createMesh(size, material):
+func createMesh(mat, size):
 	var surfaceTool = SurfaceTool.new()
 	surfaceTool.begin(Mesh.PRIMITIVE_TRIANGLES)
-	
-	# surfaceTool.add_normal(Vector3(    0,     0,  1))
-	
-	surfaceTool.set_material(material)
-
+	surfaceTool.set_material(mat)
+	surfaceTool.add_uv(Vector2(0, 0))
 	surfaceTool.add_vertex(Vector3(-size, -size,  0))
+	surfaceTool.add_uv(Vector2(1, 1))
 	surfaceTool.add_vertex(Vector3( size,  size,  0))
+	surfaceTool.add_uv(Vector2(1, 0))
 	surfaceTool.add_vertex(Vector3( size, -size,  0))
+	surfaceTool.add_uv(Vector2(0, 0))
 	surfaceTool.add_vertex(Vector3(-size, -size,  0))
+	surfaceTool.add_uv(Vector2(0, 1))
 	surfaceTool.add_vertex(Vector3(-size,  size,  0))
+	surfaceTool.add_uv(Vector2(1, 1))
 	surfaceTool.add_vertex(Vector3( size,  size,  0))
-	
 	surfaceTool.generate_normals()
-	# surfaceTool.add_color(Color(1.0, 0.0, 0.0))
-	
 	var mesh = surfaceTool.commit()
 	meshInstance.mesh = mesh
-	
+
 func _process(delta):
 	angle += delta * 30
 	meshInstance.rotation_degrees = Vector3(0, 0, angle)
