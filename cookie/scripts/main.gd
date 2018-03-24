1e3ef6fb3b159510b865d7970f675b8c3de40e77
Pete Goodwin <github@imekon.org>
2018-03-24 16:14:51 +0000

    add sprites

--- cookie/scripts/main.gd
+++ cookie/scripts/main.gd
@@ -8,13 +8,27 @@ onready var cupcakeSprite = load("res://scenes/cupcake.tscn")
 onready var danishSprite = load("res://scenes/danish.tscn")
 onready var donutSprite = load("res://scenes/donut.tscn")
 onready var macaroonSprite = load("res://scenes/macaroon.tscn")
-onready var suharcookieSprite = load("res://scenes/sugarcookie.tscn")
+onready var sugarcookieSprite = load("res://scenes/sugarcookie.tscn")
 onready var tileSprite = load("res://scenes/tile.tscn")
 
+const NumSprites = 6
 const NumRows = 9
 const NumColumns = 9
 
+enum State { Idle, Dropping, Deleting }
+
 var level
+var sprites = []
+
+class Cookie:
+	var index
+	var sprite
+	var state
+	
+	func _init(i, s):
+		index = i
+		sprite = s
+		state = State.Idle
 
 class Cell:
 	var row
@@ -47,14 +61,31 @@ class Level:
 				setCellAtRowColumn(row, column, cell)
 	
 	func getCellPosition(r, c):
-		return Vector2(c * 32 + 32, r * 36 + 200)
+		return Vector2(c * 32 + 32, r * 36 + 130)
 			
 	func getCellAtRowColumn(r, c):
 		return cells[c][r]
 	
 	func setCellAtRowColumn(r, c, cell):
 		cells[c][r] = cell
+		
+	func fillLevel(scene, sprites):
+		for row in range(NumRows):
+			for column in range(NumColumns):
+				var index = randi() % NumSprites
+				var sprite = sprites[index].instance()
+				sprite.position = getCellPosition(row, column)
+				scene.add_child(sprite)
+				var cookie = Cookie.new(index, sprite)
+				cells[column][row].cookie = cookie
 
 func _ready():
 	randomize()
+	sprites.append(croissantSprite)
+	sprites.append(cupcakeSprite)
+	sprites.append(danishSprite)
+	sprites.append(donutSprite)
+	sprites.append(macaroonSprite)
+	sprites.append(sugarcookieSprite)
 	level = Level.new(self, tileSprite)
+	level.fillLevel(self, sprites)
