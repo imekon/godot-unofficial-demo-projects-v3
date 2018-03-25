c92b24f4e11c6ac06385be0c81309e6d5195be37
Pete Goodwin <github@imekon.org>
2018-03-25 18:22:08 +0100

    update sprites to have rigid body for physics
    let physics drop the items

--- cookie/scripts/main.gd
+++ cookie/scripts/main.gd
@@ -9,13 +9,14 @@ onready var danishSprite = load("res://scenes/danish.tscn")
 onready var donutSprite = load("res://scenes/donut.tscn")
 onready var macaroonSprite = load("res://scenes/macaroon.tscn")
 onready var sugarcookieSprite = load("res://scenes/sugarcookie.tscn")
+onready var floorSprite = load("res://scenes/floor.tscn")
 onready var tileSprite = load("res://scenes/tile.tscn")
 
 const NumSprites = 6
 const NumRows = 9
 const NumColumns = 9
 
-enum State { Idle, Dropping, Deleting }
+enum State { Idle, Empty, Dropping, Deleting }
 
 var level
 var sprites = []
@@ -78,14 +79,69 @@ class Level:
 				scene.add_child(sprite)
 				var cookie = Cookie.new(index, sprite)
 				cells[column][row].cookie = cookie
+				
+	func scanForHorizontalMatch(cell):
+		var row = cell.row
+		var column = cell.column + 1
+		var index = cell.cookie.index
+		var count = 0
+		while(column < NumColumns):
+			var c = cells[row][column]
+			if c.cookie.index != index:
+				return count
+			count = count + 1
+			column = column + 1
+		return count
+		
+	func scanForVerticalMatch(cell):
+		return 0
+		
+	func markForDeleteHorizontal(r, c, count):
+		for index in range(count):
+			var cell = cells[r + index][c]
+			cell.cookie.state = State.Deleting
+			
+	func deleteMarkedCells():
+		for row in range(NumRows):
+			for column in range(NumColumns):
+				var cell = cells[row][column]
+				if cell.cookie.state == State.Deleting:
+					cell.cookie.index = -1
+					cell.cookie.state = State.Empty
+					cell.cookie.sprite.queue_free()
+				
+	func scanForMatch():
+		for row in range(NumRows - 3):
+			for column in range(NumColumns - 3):
+				var cell = cells[row][column]
+				var count
+				count = scanForHorizontalMatch(cell)
+				if count >= 3:
+					markForDeleteHorizontal(row, column, count)
+				scanForVerticalMatch(cell)
 
 func _ready():
 	randomize()
+	
+	# Add the floor instance. The sprite is invisible,
+	# as we don't need to see it. Could delete it, but left
+	# it in so you could see how I created the polygon for
+	# the static body
+	var f = floorSprite.instance()
+	f.position = Vector2(4 * 32 + 32, 4 * 36 + 130)
+	add_child(f)
+	
+	# All the instances of sprites in play
 	sprites.append(croissantSprite)
 	sprites.append(cupcakeSprite)
 	sprites.append(danishSprite)
 	sprites.append(donutSprite)
 	sprites.append(macaroonSprite)
 	sprites.append(sugarcookieSprite)
+
+	# Create the level
 	level = Level.new(self, tileSprite)
 	level.fillLevel(self, sprites)
+
+	level.scanForMatch()
+	level.deleteMarkedCells()
