d8718dae71378189c037e4e0115ce7952830be54
Pete Goodwin <github@imekon.org>
2018-05-30 20:46:17 +0100

    add alien health
    but not rotate?!?

---
+++ td2/scripts/Alien.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class Alien : Sprite
+{
+	private int health;
+	
+	public int Health
+	{
+		get { return health; }
+		set
+		{
+			health = value;
+		}
+	}
+	
+	public Alien()
+	{
+		health = 100;
+	}
+	
+    public override void _Ready()
+    {
+        // Called every time the node is added to the scene.
+        // Initialization here
+        
+    }
+
+	public override void _Draw()
+	{
+		DrawRect(new Rect2(-20, -20, 40 * health / 100, 8), new Color(255, 0, 0), true);
+	}
+}
