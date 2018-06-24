13fbb2ee2e3ad8a6c7cc0f5cc5ec8a8e2fec9981
Pete Goodwin <gitlab@imekon.org>
2018-06-24 08:24:40 +0100

    switched to physics
    rotation tricky

--- cubes/scripts/Main.cs
+++ cubes/scripts/Main.cs
@@ -3,19 +3,16 @@ using System;
 
 public class Main : Node2D
 {
+	private const float ACCELERATION = 5;
+	private const float ROTATION = 2;
+	
 	private PackedScene cube;
-	private Node2D player;
-	private Vector2 thrust;
-	private Vector2 inertia;
-	private float decay;
+	private RigidBody2D player;
 	
     public override void _Ready()
     {
 		cube = (PackedScene)ResourceLoader.Load("res://scenes/Cube.tscn");
-		player = (Node2D)GetNode("Player");
-		thrust = new Vector2(0, 0);
-		inertia = new Vector2(0, 0);
-		decay = 0.0f;
+		player = (RigidBody2D)GetNode("RigidBody2D");
 		
 		var random = new Random();
 		
@@ -37,28 +34,16 @@ public class Main : Node2D
 			RotateShip(-1);
 		else if (Input.IsActionPressed("ui_right"))
 			RotateShip(1);
-			
-		player.Position = player.Position + thrust + inertia;
-		
-		var change = decay - delta;
-		if (change > 0.0f && change < 1.0f)
-			decay = decay - delta;
-			
-		inertia = inertia * decay;
     }
 	
 	private void Thrust()
 	{
-		inertia = thrust;
-		
-		thrust.x = 1.0f;
-		thrust.y = 0.0f;
-		thrust = thrust.Rotated(player.Rotation);
-		decay = 1.0f;
+		var velocity = player.GetLinearVelocity();
+		player.ApplyImpulse(new Vector2(), new Vector2(ACCELERATION, 0).Rotated(player.Rotation));
 	}
 	
 	private void RotateShip(float offset)
 	{
-		player.Rotation += offset * 0.1f;
+		player.SetAngularVelocity(ROTATION * offset);
 	}
 }
