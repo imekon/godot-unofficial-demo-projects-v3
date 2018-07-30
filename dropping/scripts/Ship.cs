ad51732c7989d00a1f0e9ca0c21d10b19fad10f3
Pete Goodwin <gitlab@imekon.org>
2018-07-30 20:58:11 +0100

    more dropping

---
+++ dropping/scripts/Ship.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public class Ship : Area2D
+{
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+
+    public override void _Ready()
+    {
+        // Called every time the node is added to the scene.
+        // Initialization here
+        
+    }
+
+//    public override void _Process(float delta)
+//    {
+//        // Called every frame. Delta is time since last frame.
+//        // Update game logic here.
+//        
+//    }
+
+	private void OnShipAreaEntered(Godot.Object area)
+	{
+	    // Replace with function body
+	}
+}
+
